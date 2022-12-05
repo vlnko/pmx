882c1a1fa768839623955892acff26552f1858a4
vlnko <laslolnko@gmail.com>
2022-12-05 14:58:43 +0300

    created projects list view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProjectCreateView
+from .views import ProjectCreateView, ProjectList
 
 
 urlpatterns = [
     path("create", ProjectCreateView.as_view(), name="project-create"),
+    path("all", ProjectList.as_view(), name="project-all"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Project
 
 
 from .forms import ProjectCreateForm
@@ -9,5 +11,10 @@ from .forms import ProjectCreateForm
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     form_class = ProjectCreateForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("project-all")
     template_name = "projects/projectcreate.html"
+
+
+class ProjectList(LoginRequiredMixin, ListView):
+    model = Project
+    context_object_name = 'projects'
