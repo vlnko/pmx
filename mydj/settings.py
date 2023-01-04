86b9d4adca50a6d7bc6fdc07120d376f8e23c3c7
vlnko <laslolnko@gmail.com>
2023-01-04 13:57:22 +0300

    cors fixes for api

--- mydj/settings.py
+++ mydj/settings.py
@@ -15,6 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -133,5 +134,8 @@ LOGOUT_REDIRECT_URL = "project-home"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
 }
